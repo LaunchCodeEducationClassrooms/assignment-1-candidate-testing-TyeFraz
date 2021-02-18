38511cd9adb81adaef490cf49e63e3e3f4ea61ce
TyeFraz <tcolettefrazier@gmail.com>
2021-02-18 03:10:28 +0000

    trying True as true

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,7 @@ let question="Who was the first American woman in space?";
 let correctAnswer="Sally Ride";
 let candidateAnswer=("");
 let questions=[ '1) Who was the first American woman in space?', '2)True or false: 5000 meters == 5 kilometers?', '3) (5+3)/2*10=?', '4)Given the array [8,"Orbit","Trajectory",45]. what entry is at index 2?', '5) What is the minimum crew size for ISS?' ]
-let correctAnswers= ["Sally Ride","True","40","Trajectory","3"];
+let correctAnswers= ["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers=[];
 let numberOfCorrectAnswers=0
 let grade=0
@@ -36,11 +36,11 @@ function gradeQuiz(candidateAnswer,correctAnswer) {
   if (candidateAnswer===correctAnswer.toLowerCase()){
   numberOfCorrectAnswers +=1
   
-    return ((grade=numberOfCorrectAnswers),(console.log(`Your Answer: ${candidateAnswer}\n Correct Answer: ${correctAnswer}`)));
+    return ((grade=numberOfCorrectAnswers),(console.log(`Your Answer: ${candidateAnswer}\nCorrect Answer: ${correctAnswer}`)));
 
 }
 return ((grade=numberOfCorrectAnswers),
- (console.log(`Your Answer: ${candidateAnswer} \n Correct Answer: ${correctAnswer}`)),
+ (console.log(`Your Answer: ${candidateAnswer} \nCorrect Answer: ${correctAnswer}`)),
 grade);
   return grade
 
